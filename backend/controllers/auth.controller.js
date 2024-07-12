3bb13cbca014df6c1243ef406a50c8a4edab13f9
swarat <swaratvaghela@gmail.com>
2024-07-12 11:26:46 +0530

    Login Logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -60,15 +60,49 @@ const signup = async(req, res) => {
 
 const login = async(req, res) => {
   try {
-    
+    const {username, password}=req.body;
+
+    const user= await User.findOne({username});
+    const isPasswordCorrect = await bcryptjs.compare(password, user?.password||"");
+
+    if(!user || !isPasswordCorrect){
+      return res.status(400).json({error:"Internal username or Password"});
+    }
+
+    generateTokenandCookie(user._id, res);
+
+    res.status(200).json({
+      _id:user._id,
+      fullName:user.fullName,
+      username:user.username,
+      profilePic:user.profilePic
+
+    });
   } catch (error) {
+
+    console.log("Error in login controller", error.message);
+    res.status(500).json({error:"Internal Server Error"});
     
   }
 };
 
 
-const logout = (req, res) => {
-  console.log("logout");
+
+
+
+
+
+const logout = async(req, res) => {
+
+  try {
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:"Logged out successfully"});
+  } catch (error) {
+    
+    console.log("Error in login controller", error.message);
+    res.status(500).json({error:"Internal Server Error"});
+
+  }
 };
 
 module.exports = {
