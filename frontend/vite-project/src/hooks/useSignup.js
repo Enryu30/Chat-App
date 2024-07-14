7ade5e9050b7d83f6ddcae1293e41bd29bb9427e
swarat <swaratvaghela@gmail.com>
2024-07-14 16:59:30 +0530

    Signup functionality Successfully implemented yeahhhh

---
+++ frontend/vite-project/src/hooks/useSignup.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+
+
+const useSignup = ()=>{
+  const[loading, setLoading]=useState(false);
+
+  const signup = async({fullName, username, password, confirmPassword, gender}) =>{
+
+    const success = handleInputErrors({fullName, username, password, confirmPassword, gender});
+
+    if(!success)return;
+
+    setLoading(true);
+
+    
+    try {
+			const res = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({fullName, username, password, confirmPassword, gender})
+			});
+
+			const data = await res.json();
+			console.log(data);
+		} catch (error) {
+			console.error("Error during sign up:", error);
+		}
+    finally{setLoading(false);}
+
+  }
+
+  return {loading,signup};
+}
+
+export default useSignup;
+
+const handleInputErrors =({fullName, username, password, confirmPassword, gender})=>{
+
+  if(!fullName || !username || !password || !confirmPassword || !gender){
+    toast.error('Please fill in all fields');
+    return false;
+  }
+
+  if(password!=confirmPassword){
+    toast.error('Passwords do not match');
+    return false;
+  }
+
+  if(password.Length < 6){
+    toast.error('Password must be atleast 6 characters');
+    return false;
+  }
+
+  return true;
+}
+
+
+//use signuup is called which return loading and signup functtion
+//then when the handlesubmit button is clicked signup function is called
